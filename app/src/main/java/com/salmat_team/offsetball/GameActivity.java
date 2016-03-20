811e0dc85e60f60518f566de76ca0129a7a13450
adamita <adamita22@gmail.com>
2016-03-20 20:28:46 +0100

    Activity added

---
+++ app/src/main/java/com/salmat_team/offsetball/GameActivity.java
@@ -0,0 +1,23 @@
+package com.salmat_team.offsetball;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Created by adamita on 2016. 03. 20..
+ */
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_layout);
+        }
+    }
--- app/src/main/java/com/salmat_team/offsetball/MainActivity.java
+++ app/src/main/java/com/salmat_team/offsetball/MainActivity.java
@@ -1,7 +1,9 @@
 package com.salmat_team.offsetball;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButtonClick(View v)
+    {
+        if(v.getId()==R.id.StartButton)
+        {
+            Intent i=new Intent(MainActivity.this,GameActivity.class);
+            startActivity(i);
+        }
+    }
+
 }
